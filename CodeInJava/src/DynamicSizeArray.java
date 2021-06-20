d85de1103681f9d5661dfcb0eaf67f10d2262328
Nannf <akmd_szc@163.com>
2021-06-20 22:31:39 +0800

    动态数组

---
+++ CodeInJava/src/DynamicSizeArray.java
@@ -0,0 +1,14 @@
+/**
+ * 动态扩容数组
+ * @author Nannf
+ * @date 2021/6/20 22:28
+ * @description
+ */
+public class DynamicSizeArray {
+
+    // data
+    private Object[] data;
+
+    // data count
+    private int size;
+}
---
+++ DailyThinkRecord/src/main/java/array/DynamicSizeArray.java
@@ -0,0 +1,16 @@
+package array;
+
+/**
+ * 动态扩容数组
+ * @author Nannf
+ * @date 2021/6/20 22:28
+ * @description
+ */
+public class DynamicSizeArray {
+
+    // data
+    private Object[] data;
+
+    // data count
+    private int size;
+}
