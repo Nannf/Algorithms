7d6ef65275b9038c939954e457688a4ef5f02dfb
Nannf <akmd_szc@163.com>
2021-06-22 17:37:39 +0800

    thread wait notify modify

--- DailyThinkRecord/src/main/java/thread/SimpleBlockingQueue.java
+++ DailyThinkRecord/src/main/java/thread/SimpleBlockingQueue.java
@@ -12,19 +12,21 @@ import java.util.concurrent.locks.ReentrantLock;
 public class SimpleBlockingQueue<E> {
     private LinkedList<E> data;
     private final ReentrantLock lock = new ReentrantLock();
-    private Condition waitFullCondition = lock.newCondition();
-    private Condition waitEmptyCondition = lock.newCondition();
+    private Condition waitSpaceToAddCondition = lock.newCondition();
+    private Condition waitDataToGetCondition = lock.newCondition();
     private static final int MAX_SIZE = 10;
 
 
     public E get() {
         try {
             lock.lock();
+            // 当是空的时候
             while (data.isEmpty()) {
-                waitFullCondition.await();
+                waitDataToGetCondition.await();
             }
             E e = data.getFirst();
-            waitEmptyCondition.notifyAll();
+            // 取完之后，就可以唤醒所有在等着空间加数据的线程了
+            waitSpaceToAddCondition.notifyAll();
             return e;
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,16 +39,18 @@ public class SimpleBlockingQueue<E> {
     public void set(E e) {
         try {
             lock.lock();
+            // 第一次执行的时候
             if (data == null) {
                 LinkedList<E> linkedList = new LinkedList<>();
                 linkedList.add(e);
                 return;
             }
+            // 队列满了
             while (data.size() ==  MAX_SIZE) {
-                waitEmptyCondition.await();
+                waitSpaceToAddCondition.await();
             }
             data.add(e);
-            waitFullCondition.notifyAll();
+            waitDataToGetCondition.notifyAll();
         } catch (Exception ee) {
             ee.printStackTrace();
         } finally {
