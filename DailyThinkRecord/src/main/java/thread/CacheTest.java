e9f53f2caf6092d43ea5886d21dfd856c8553097
Nannf <akmd_szc@163.com>
2021-06-29 18:58:27 +0800

    Read Write Lock Cache2

--- DailyThinkRecord/src/main/java/thread/CacheTest.java
+++ DailyThinkRecord/src/main/java/thread/CacheTest.java
@@ -9,27 +9,53 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @date 2021/6/29 18:45
  * @description
  */
-public class CacheTest<K,V> {
-    Map<K,V> cache = new HashMap<>();
+public class CacheTest<K, V> {
+    Map<K, V> cache = new HashMap<>();
 
     ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
     ReentrantReadWriteLock.WriteLock writeLock = lock.writeLock();
     ReentrantReadWriteLock.ReadLock readLock = lock.readLock();
 
     V get(K key) {
+        V v = null;
         try {
             readLock.lock();
-            return cache.get(key);
-        }finally {
+
+            v = cache.get(key);
+        } finally {
             readLock.unlock();
         }
+        // 缓存直接命中
+        if (v != null) {
+            return v;
+        }
+
+        writeLock.lock();
+
+        try {
+            v = cache.get(key);
+
+            if ( v == null) {
+                // 省略了从数据库中加载数据的代码
+                // loadData
+                v = null;
+                cache.put(key,v);
+            }
+            return v;
+
+        } finally {
+            writeLock.unlock();
+        }
+
+
+
     }
 
     void put(K key, V value) {
         try {
             writeLock.lock();
-            cache.put(key,value);
-        }finally {
+            cache.put(key, value);
+        } finally {
             writeLock.unlock();
         }
     }
