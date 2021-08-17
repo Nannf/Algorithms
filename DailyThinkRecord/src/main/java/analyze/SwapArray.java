5655e5e6fc749f91cd5cc871100d5df51295bbf0
Nannf <NannfTime@outlook.com>
2021-08-17 20:20:34 +0800

    swap array

---
+++ DailyThinkRecord/src/main/java/analyze/SwapArray.java
@@ -0,0 +1,16 @@
+package analyze;
+
+/**
+ * @author Nannf
+ * @version v1.0
+ * @Description
+ * @date 2021/8/17 18:41
+ *
+ * 给定数组a,给定一个值i，把i之后（不包括i）和i之前的元素替换了，但是保证每半边的元素相对顺序不变
+ * 例如：
+ * 现有数组a[1,2,3,4,5,6,7],和i=3
+ * 输出
+ * 5，6，7，1，2，3，4
+ */
+public class SwapArray {
+}
