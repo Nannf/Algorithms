eac5a4f03d863e3cb0b7ccf680547cb8325f6373
Nannf <NannfTime@outlook.com>
2021-08-22 20:21:54 +0800

    最长子串的n^2

---
+++ DailyThinkRecord/src/main/java/analyze/JobScheduling.java
@@ -0,0 +1,14 @@
+package analyze;
+
+/**
+ * @author Nannf
+ * @version v1.0
+ * @Description  工作调度问题
+ * @date 2021/8/21 20:33
+ */
+public class JobScheduling {
+
+
+    public static void main(String[] args) {
+    }
+}
