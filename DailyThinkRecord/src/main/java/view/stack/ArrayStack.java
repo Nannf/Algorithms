3d44b20fccf3c8b24fc37862387bc3b57fd53057
Nannf <NannfTime@outlook.com>
2021-08-30 15:59:29 +0800

    栈

---
+++ DailyThinkRecord/src/main/java/view/stack/ArrayStack.java
@@ -0,0 +1,9 @@
+package view.stack;
+/**
+ * @author Nannf
+ * @version v1.0
+ * @Description 数组实现的栈
+ * @date 2021/8/30 14:17
+ */
+public class ArrayStack {
+}
---
+++ DailyThinkRecord/src/main/java/view/stack/LinkedStack.java
@@ -0,0 +1,10 @@
+package view.stack;
+
+/**
+ * @author Nannf
+ * @version v1.0
+ * @Description 链表实现的栈
+ * @date 2021/8/30 14:18
+ */
+public class LinkedStack {
+}
