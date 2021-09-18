ae0dfc2d51dad596bdd13eb737f3260cc5d0362c
nannf <akmd_szc@163.com>
2021-09-18 22:29:54 +0800

    再谈递归

--- DailyThinkRecord/src/main/java/sort/MergeSort.java
+++ DailyThinkRecord/src/main/java/sort/MergeSort.java
@@ -29,17 +29,65 @@ public class MergeSort {
      * }
      */
 
+    public static void main(String[] args) {
+        int[] a = {6, 5, 4, 1, 2, 3};
+        sort(a, 6);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
 
     public static void sort(int[] a, int len) {
         subSort(a, 0, len - 1);
     }
 
+    // 递归的绝大多数工作就是在给出递归函数的定义
+    // 对本例而言，这个定义就是对数组a中的start 和 end 进行排序，我们对外给出了这个申明
     private static void subSort(int[] a, int start, int end) {
+        // 照例给出终止条件，终止条件的含义就是，当start == end 的时候，天然有序； 当 start > end 的时候，此时已然不合法的状态。
+        // 这就是终止状态，一般一个合理的子问题最后都会变成一个数字
         if (start >= end) {
             return;
         }
+        // 然后是如何拆分子问题
         int mid = (start + end) / 2;
+        // 我把左边的排有序了
         subSort(a, start, mid);
+        // 我把右边的排有序了
         subSort(a, mid + 1, end);
+        // 合并左右有序的，那么就全有序了
+        merge(a, start, mid, end);
+    }
+
+    private static void merge(int[] a, int start, int mid, int end) {
+        // 这个函数就是假设 a[start,mid]和a[mid+1,end]分别有序，然后使 a[start,end]有序
+        int[] temp = new int[end - start + 1];
+
+        int i = start;
+        int j = mid + 1;
+
+        int t = 0;
+
+        while (i <= mid && j <= end) {
+            if (a[i] <= a[j]) {
+                temp[t++] = a[i++];
+            } else {
+                temp[t++] = a[j++];
+            }
+        }
+
+        if (i <= mid) {
+            temp[t++] = a[i++];
+        }
+
+        if (j <= end) {
+            temp[t++] = a[j++];
+        }
+
+        t = 0;
+        for (int m = start; m <= end; m++) {
+            a[m] = temp[t++];
+        }
     }
 }
+
