28059494f9a1d96b78a9f0bf64ce5c5a8d0c6b53
nannf <akmd_szc@163.com>
2021-10-11 23:01:32 +0800

    kotlin day 2

--- DailyThinkRecord/src/main/java/learnkotlin/LearnKotlin.kt
+++ DailyThinkRecord/src/main/java/learnkotlin/LearnKotlin.kt
@@ -1,5 +1,23 @@
 package learnkotlin
 
+import kotlin.math.max
+
 fun main() {
     println(" Hello Kotlin")
+    val a = 10
+    println(a)
+
+    // val = final
+//    val b: Int = 10
+    var b: Int = 10
+    b = b*10
+    println(b)
+
+    println("max " + largerNumber2(a,b))
 }
+
+fun largerNumber(num1: Int, num2: Int): Int {
+    return max(num1,num2)
+}
+
+fun largerNumber2(num1: Int, num2: Int): Int = max(num1,num2)
